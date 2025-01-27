cc6c8ceb1fa75a8a99e74e53f2bb5b9ced9858ea
ActorXXX <jose_rios@outlook.com>
2025-01-26 20:29:28 -0500

    las imagenes del carousel deben estar alojadas en la nube, corregido

--- src/assets/images/imgData.ts
+++ src/assets/images/imgData.ts
@@ -1,17 +1,17 @@
 export const imgData = [
     {
-      id: 1, imgUrl: "src/assets/images/carousel1.jpg"
+      id: 1, imgUrl: "https://www.lomino.net.pe/img/carousel1.jpg"
     },
     {
-      id: 2, imgUrl: "src/assets/images/carousel2.jpg"
+      id: 2, imgUrl: "https://www.lomino.net.pe/img/carousel2.jpg"
     },
     {
-      id: 3, imgUrl: "src/assets/images/carousel3.jpg"
+      id: 3, imgUrl: "https://www.lomino.net.pe/img/carousel3.jpg"
     },
     {
-      id: 4, imgUrl: "src/assets/images/carousel4.jpg"
+      id: 4, imgUrl: "https://www.lomino.net.pe/img/carousel4.jpg"
     },
     {
-      id: 5, imgUrl: "src/assets/images/carousel5.jpg"
+      id: 5, imgUrl: "https://www.lomino.net.pe/img/carousel5.jpg"
     }
   ];
